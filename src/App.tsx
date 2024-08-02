b95c611089a4ef06156740682c3a017c9f2b9131
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-08-03 02:04:09 +0530

    add login flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
 import Home from './components/home/Home';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthContextProvider } from './components/hooks/AuthContext';
+import ProtectedRoute from './components/utils/ProtectedRoute';
+
+const Login = React.lazy(() => import('./components/login/Login'));
+const Register = React.lazy(() => import('./components/register/Register'));
+const Dashboard = React.lazy(() => import('./components/dashboard/Dashboard'));
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Home />
+        <AuthContextProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/login"
+              element={
+                <React.Suspense fallback={<></>}>
+                  <Login />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <React.Suspense fallback={<></>}>
+                  <Register />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <React.Suspense fallback={<></>}>
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                </React.Suspense>
+              }
+            />
+          </Routes>
+        </AuthContextProvider>
       </BrowserRouter>
     </div>
   );
