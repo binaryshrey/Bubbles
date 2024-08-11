d1cafb487188b9152ec7df5405271d3d67cfe72b
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-08-11 23:26:37 +0530

    add modal image carousel

--- src/components/album/albumLayout/LayoutOne.jsx
+++ src/components/album/albumLayout/LayoutOne.jsx
@@ -1,11 +1,26 @@
 /************************************************************ IMPORTS ************************************************************/
 
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logoLight from '../../../assets/logo-light.svg';
+import { ReactImageCarouselViewer } from 'react-image-carousel-viewer';
 
 /************************************************************ IMPORTS ************************************************************/
 
 const LayoutOne = ({ albumData }) => {
+  const [index, setIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const albumImages = albumData?.album_photos.map((item) => ({ src: item }));
+
+  useEffect(() => {
+    // hack to remove the top-0 class from the img elements to show the image carousel properly
+    const imgElements = document.querySelectorAll('img');
+    imgElements.forEach((img) => {
+      img.classList.remove('md:top-0');
+    });
+  }, [isOpen]);
+
   return (
     <div>
       <div className="max-w-4xl mx-auto md:px-32 px-8 flex flex-col justify-center">
@@ -14,7 +29,17 @@ const LayoutOne = ({ albumData }) => {
           <div className="flex h-full w-full items-center justify-center">
             <div className="grid h-full w-full grid-cols-8 grid-rows-8 ">
               <div className="col-span-8 row-span-8 flex items-center justify-center">
-                <img src={albumData?.album_photos[0]} alt="Pic 1" className="w-full h-full object-cover rounded-xl" />;
+                <img
+                  src={albumData?.album_photos[0]}
+                  alt="Pic 1"
+                  className="w-full h-full object-cover rounded-xl cursor-pointer"
+                  onClick={() => {
+                    setIndex(0);
+                    setIsOpen(true);
+                  }}
+                />
+
+                <ReactImageCarouselViewer open={isOpen} onClose={() => setIsOpen(false)} images={albumImages} startIndex={index} />
               </div>
             </div>
           </div>
@@ -22,8 +47,8 @@ const LayoutOne = ({ albumData }) => {
       </div>
       <div className="m-4">
         <Link to="https://bubbles-inc.vercel.app/" target="_blank" rel="noopener noreferrer">
-          <div class="fixed bottom-0 right-0 m-4">
-            <div class="bg-zinc-800 flex rounded-full items-center">
+          <div className="fixed bottom-0 right-0 m-4">
+            <div className="bg-zinc-800 flex rounded-full items-center">
               <img src={logoLight} alt="Bubbles" className="h-3 w-auto ml-2" />
               <p className="text-white  px-2.5 py-1 shadow text-xs ">Made with Bubbles</p>
             </div>
--- src/components/album/albumLayout/MasonryLayout.jsx
+++ src/components/album/albumLayout/MasonryLayout.jsx
@@ -2,11 +2,26 @@
 
 import { Link } from 'react-router-dom';
 import Masonry from 'react-layout-masonry';
+import { useState, useEffect } from 'react';
 import logoLight from '../../../assets/logo-light.svg';
+import { ReactImageCarouselViewer } from 'react-image-carousel-viewer';
 
 /************************************************************ IMPORTS ************************************************************/
 
 const MasonryLayout = ({ albumData }) => {
+  const [index, setIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const albumImages = albumData?.album_photos.map((item) => ({ src: item }));
+
+  useEffect(() => {
+    // hack to remove the top-0 class from the img elements to show the image carousel properly
+    const imgElements = document.querySelectorAll('img');
+    imgElements.forEach((img) => {
+      img.classList.remove('md:top-0');
+    });
+  }, [isOpen]);
+
   return (
     <div>
       <div className="max-w-4xl mx-auto md:px-32 px-8 ">
@@ -14,15 +29,25 @@ const MasonryLayout = ({ albumData }) => {
         <div className="mt-8">
           <Masonry columns={{ 640: 1, 768: 2 }} className="flex -m-2" columnclassname="m-2" gap={8}>
             {albumData?.album_photos?.map((url, index) => (
-              <img key={index} src={url} alt={`Pic ${index}`} className="w-full h-auto object-cover rounded text-zinc-700" />
+              <img
+                key={index}
+                src={url}
+                alt={`Pic ${index}`}
+                className="w-full h-auto object-cover rounded text-zinc-700 cursor-pointer"
+                onClick={() => {
+                  setIndex(0);
+                  setIsOpen(true);
+                }}
+              />
             ))}
           </Masonry>
+          <ReactImageCarouselViewer open={isOpen} onClose={() => setIsOpen(false)} images={albumImages} startIndex={index} />
         </div>
       </div>
       <div className="m-4">
         <Link to="https://bubbles-inc.vercel.app/" target="_blank" rel="noopener noreferrer">
-          <div class="fixed bottom-0 right-0 m-4">
-            <div class="bg-zinc-800 flex rounded-full items-center">
+          <div className="fixed bottom-0 right-0 m-4">
+            <div className="bg-zinc-800 flex rounded-full items-center">
               <img src={logoLight} alt="Bubbles" className="h-3 w-auto ml-2" />
               <p className="text-white  px-2.5 py-1 shadow text-xs ">Made with Bubbles</p>
             </div>
