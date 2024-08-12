b905006046decd5d0850d695d50d8adaccf0d430
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-08-12 16:25:21 +0530

    add analytics charts

--- src/components/analytics/Analytics.jsx
+++ src/components/analytics/Analytics.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import AnalyticsOverview from './AnalyticsOverview';
+import AlbumAnalytics from './AlbumAnalytics';
 
 const Analytics = () => {
   return (
-    <div>
+    <div className="bg-black">
+      <p className="text-white font-semibold text-xl md:text-2xl mb-4">Overview</p>
       <AnalyticsOverview />
+      <div className="flex flex-shrink-0 border-t border-zinc-800 mt-4 mb-4"></div>
+      <p className="text-white font-semibold text-xl md:text-2xl mt-4 mb-4">Albums</p>
+      <AlbumAnalytics />
     </div>
   );
 };
